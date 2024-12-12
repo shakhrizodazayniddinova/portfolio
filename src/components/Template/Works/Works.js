540e4f6366478467a86598306052ae2fe642f88d
Your Name <shahrizodazayniddinova7@gmail.com>
2024-12-12 11:36:01 +0500

    my portfolio

--- src/components/Template/Works/Works.js
+++ src/components/Template/Works/Works.js
@@ -4,54 +4,34 @@ import { Box, Grid, Typography } from '@mui/material';
 import { WorksStyled } from './WorksStyled';
 import 'animate.css';
 
-import treloo from './Assets/treloo.png';
-import hilltop from './Assets/hilltop.png';
-import foodhut from './Assets/foodhut.png';
-import shopbootstrap from './Assets/shopbootstrap.png';
-import welcomebootstrap from './Assets/welcomebootstrap.png';
-import olegmarkov from './Assets/olegmarkov.png';
-import kaylagriffith from './Assets/kaylagriffith.png';
-import geeks from './Assets/geeks.png';
-import albumlayout from './Assets/albumlayout.png';
-import calculator from './Assets/calculator.png';
-import todo from './Assets/todo.png';
-import formdemo from './Assets/formdemo.png';
-import organicfood from './Assets/organicfood.png';
-import zone from './Assets/zone.png';
-import charts from './Assets/charts.png';
-import skymovie from './Assets/skymovie.png';
-import booknest from './Assets/booknest.png';
-import express from './Assets/express.png';
-import resipe from './Assets/resipe.png';
-import youtube from './Assets/youtube.png';
-import calendar from './Assets/calendar.png';
+import { designCardData, dynamicCardData } from './WorksDatas';
 
-const designCardData = [
-    {img: treloo, workName: 'Treloo', aboutWork: 'HTML | SCSS | Responsive', link: 'https://trelooo.netlify.app/', sourceCodeClass: 'sourceCodeNone'},
-    {img: hilltop, workName: 'Hill Top', aboutWork: 'HTML | SCSS | Not responsive', link: 'https://hilltop-shakhrizoda.netlify.app', sourceCodeClass: 'sourceCodeNone'},
-    {img: foodhut, workName: 'Food Hut', aboutWork: 'HTML | SCSS | JavaScript | Responsive', link: 'https://food-hut-by-shakhrizoda.netlify.app', sourceCodeClass: 'sourceCodeNone'},
-    {img: shopbootstrap, workName: 'Start Bootstrap', aboutWork: 'React | React Bootstrap | Not Responsive', link: 'https://homepage-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/shopHomePage', sourcecode: 'Source Code'},
-    {img: zone, workName: 'Zone Landing', aboutWork: 'React | MUI | Styled components | Not Responsive', link: 'https://zone-landing-clone.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/zone-landing-clone', sourcecode: 'Source Code'},
-    {img: welcomebootstrap, workName: 'Blog Home', aboutWork: 'React | React Bootstrap | Not Responsive', link: 'https://welcome-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/welcomeMyApp', sourcecode: 'Source Code'},
-    {img: olegmarkov, workName: 'Oleg Markov', aboutWork: 'React | CSS | Not Responsive', link: 'https://olegmarkov-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/oleg', sourcecode: 'Source Code'},
-    {img: kaylagriffith, workName: 'Kayla Griffith', aboutWork: 'React | CSS | Not Responsive', link: 'https://kaylagriffith-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/griffith', sourcecode: 'Source Code'},
-    {img: geeks, workName: 'Geeks', aboutWork: 'React | Styled components | Not Responsive', link: 'https://geeks-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/geeks', sourcecode: 'Source Code'},
-    {img: albumlayout, workName: 'Album layout', aboutWork: 'React | Material UI | Responsive', link: 'https://album-layout-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/album-layout', sourcecode: 'Source Code'},
-];
+// const designCardData = [
+//     {img: treloo, workName: 'Treloo', aboutWork: 'HTML | SCSS | Responsive', link: 'https://trelooo.netlify.app/', sourceCodeClass: 'sourceCodeNone'},
+//     {img: hilltop, workName: 'Hill Top', aboutWork: 'HTML | SCSS | Not responsive', link: 'https://hilltop-shakhrizoda.netlify.app', sourceCodeClass: 'sourceCodeNone'},
+//     {img: foodhut, workName: 'Food Hut', aboutWork: 'HTML | SCSS | JavaScript | Responsive', link: 'https://food-hut-by-shakhrizoda.netlify.app', sourceCodeClass: 'sourceCodeNone'},
+//     {img: shopbootstrap, workName: 'Start Bootstrap', aboutWork: 'React | React Bootstrap | Not Responsive', link: 'https://homepage-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/shopHomePage', sourcecode: 'Source Code'},
+//     {img: welcomebootstrap, workName: 'Blog Home', aboutWork: 'React | React Bootstrap | Not Responsive', link: 'https://welcome-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/welcomeMyApp', sourcecode: 'Source Code'},
+//     {img: olegmarkov, workName: 'Oleg Markov', aboutWork: 'React | CSS | Not Responsive', link: 'https://olegmarkov-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/oleg', sourcecode: 'Source Code'},
+//     {img: kaylagriffith, workName: 'Kayla Griffith', aboutWork: 'React | CSS | Not Responsive', link: 'https://kaylagriffith-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/griffith', sourcecode: 'Source Code'},
+//     {img: geeks, workName: 'Geeks', aboutWork: 'React | Styled components | Not Responsive', link: 'https://geeks-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/geeks', sourcecode: 'Source Code'},
+//     {img: albumlayout, workName: 'Album layout', aboutWork: 'React | MUI | Responsive', link: 'https://album-layout-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/album-layout', sourcecode: 'Source Code'},
+//     {img: zone, workName: 'Zone Landing', aboutWork: 'React | MUI | Styled components | Not Responsive', link: 'https://zone-landing-clone.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/zone-landing-clone', sourcecode: 'Source Code'},
+// ];
 
-const dynamicCardData = [
-    {img: calculator, workName: 'Calculator', aboutWork: 'React | SCSS | Not Responsive', link: 'https://calculator-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/calculator', sourcecode: 'Source Code'},
-    {img: youtube, workName: 'Youtube clone', aboutWork: 'React | SCSS | Material UI | Json API | Redux | Router | Not Responsive', link: 'https://youtube-by-shakhrizoda.netlify.app//', githubLink: 'https://github.com/shakhrizodazayniddinova/youtube', sourcecode: 'Source Code'},
-    {img: todo, workName: 'ToDo App', aboutWork: 'React | Styled components | Redux Toolkit | React Animation Reveal | Responsive', link: 'https://todoapp-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/todoapp', sourcecode: 'Source Code'},
-    {img: formdemo, workName: 'Form Demo', aboutWork: 'React | MUI | React Hook Form | Responsive', link: 'https://formdemo-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/formdemo', sourcecode: 'Source Code'},
-    {img: organicfood, workName: 'Organic Food', aboutWork: 'Webpack | Jsx | SCSS | Not Responsive', link: 'https://organicfood-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/organicFood', sourcecode: 'Source Code'},
-    {img: charts, workName: 'Charts', aboutWork: 'React | React ChartJS | Responsive', link: 'https://charts-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/charts', sourcecode: 'Source Code'},
-    {img: skymovie, workName: 'Sky and Movie', aboutWork: 'React | Styled components | API | Router | Responsive', link: 'https://skyandscreen-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/weather', sourcecode: 'Source Code'},
-    {img: booknest, workName: 'Book Nest', aboutWork: 'React | Context API | Router DOM | Redux | Router | Styled components | Responsive', link: 'https://booknest-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/BookNest', sourcecode: 'Source Code'},
-    {img: resipe, workName: 'Food Resipes', aboutWork: 'React | MUI | Styled components | API | Responsive', link: 'https://foodresipe.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/foodresipe', sourcecode: 'Source Code'},
-    {img: express, workName: 'Express24', aboutWork: 'React | Router DOM | Redux | Router | Styled components | Not Responsive', link: 'https://express-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/express24', sourcecode: 'Source Code'},
-    {img: calendar, workName: 'Calendar', aboutWork: 'Webpack | Jsx | SCSS | MomentJs | Classnames | React Animation Reveal | Context API | Not Responsive', link: 'https://calendar-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/calendar', sourcecode: 'Source Code'},
-];
+// const dynamicCardData = [
+//     {img: calculator, workName: 'Calculator', aboutWork: 'React | SCSS | Not Responsive', link: 'https://calculator-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/calculator', sourcecode: 'Source Code'},
+//     {img: todo, workName: 'ToDo App', aboutWork: 'React | Styled components | Redux Toolkit | React Animation Reveal | Responsive', link: 'https://todoapp-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/todoapp', sourcecode: 'Source Code'},
+//     {img: skymovie, workName: 'Sky and Movie', aboutWork: 'React | Styled components | API | Router | Responsive', link: 'https://skyandscreen-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/weather', sourcecode: 'Source Code'},
+//     {img: booknest, workName: 'Book Nest', aboutWork: 'React | Styled components | Context API | Router | Redux | Responsive', link: 'https://booknest-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/BookNest', sourcecode: 'Source Code'},
+//     {img: express, workName: 'Express24', aboutWork: 'React | Styled components | Redux | Router | Not Responsive', link: 'https://express-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/express24', sourcecode: 'Source Code'},
+//     {img: resipe, workName: 'Food Resipes', aboutWork: 'React | MUI | Styled components | API | Responsive', link: 'https://foodresipe.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/foodresipe', sourcecode: 'Source Code'},
+//     {img: formdemo, workName: 'Form Demo', aboutWork: 'React | MUI | React Hook Form | Responsive', link: 'https://formdemo-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/formdemo', sourcecode: 'Source Code'},
+//     {img: youtube, workName: 'Youtube clone', aboutWork: 'React | SCSS | MUI | Json API | Redux | Router | Not Responsive', link: 'https://youtube-by-shakhrizoda.netlify.app//', githubLink: 'https://github.com/shakhrizodazayniddinova/youtube', sourcecode: 'Source Code'},
+//     {img: charts, workName: 'Charts', aboutWork: 'React | React ChartJS | Responsive', link: 'https://charts-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/charts', sourcecode: 'Source Code'},
+//     {img: organicfood, workName: 'Organic Food', aboutWork: 'Webpack | Jsx | SCSS | Not Responsive', link: 'https://organicfood-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/organicFood', sourcecode: 'Source Code'},
+//     {img: calendar, workName: 'Calendar', aboutWork: 'Webpack | Jsx | SCSS | MomentJs | Classnames | React Animation Reveal | Context API | Not Responsive', link: 'https://calendar-by-shakhrizoda.netlify.app/', githubLink: 'https://github.com/shakhrizodazayniddinova/calendar', sourcecode: 'Source Code'},
+// ];
 
 export default function Works() {
     const [activeSection, setActiveSection] = useState("static");
@@ -99,6 +79,7 @@ export default function Works() {
                 </Grid>
             ))}
           </>;
+
         default:
             return <div className="design"></div>;
         }
