87c09430763e4eac0e9bcd823ff1b8639619c534
Your Name <shahrizodazayniddinova7@gmail.com>
2025-01-04 18:50:59 +0500

    Added Vercel Speed ​​Insights

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { Box } from "@mui/material";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Template from "./components/Template/Template";
 
 function App() {
   return (
+    <>
     <Template/>
+    <SpeedInsights />
+    </>
   );
 }
 
