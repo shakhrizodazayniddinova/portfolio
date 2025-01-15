975762e5dea9db80a92dd2fb4c185d2cb08d2651
Your Name <shahrizodazayniddinova7@gmail.com>
2025-01-15 17:19:56 +0500

    added loader

--- src/components/Template/Works/WorkCard.js
+++ src/components/Template/Works/WorkCard.js
@@ -1,14 +1,21 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import 'animate.css';
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, Skeleton, Typography } from '@mui/material';
 
 function WorkCard({items}) {
+  const [loading, setLoading] = useState(true);
+
+  const handleImageLoad = () => setLoading(false);
+
   return (
     <Grid item xs={12} sm={6} md={4} className={`animate__animated animate__rotateInUpRight`}>
         <Box className='workCard'>
             <Box className='workCardImgBox'>
+                {loading && (
+                    <Skeleton variant="rectangular" width="100%" height={200} animation="wave" />
+                )}
                 <a href={items.link} target="_blank" rel="noopener noreferrer">
-                    <img src={items.img} loading="lazy" alt='project img' />
+                    <img src={items.img} loading="lazy" alt='project img' onLoad={handleImageLoad} style={{ display: loading ? 'none' : 'block' }} />
                 </a>
                 <Typography variant='caption'>
                     <a href={items.githubLink} target="_blank" rel="noopener noreferrer" className={`${items.sourceCodeClass ? items.sourceCodeClass : 'sourceCode'}`}>
